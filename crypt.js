78831409fd233b6951e1c9aaee126f9d103a486d
R3tr0Mu4z <31041715+R3tr0Mu4z@users.noreply.github.com>
2019-02-20 02:28:24 +0600

    Create crypt.js

---
+++ crypt.js
@@ -0,0 +1,33 @@
+var crypto = require('crypto'),
+  algorithm = 'aes-256-gcm',
+  password = '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+  iv = 'thisisapassword135353453'
+
+function encrypt(text) {
+  var cipher = crypto.createCipheriv(algorithm, password, iv)
+  var encrypted = cipher.update(text, 'utf8', 'hex')
+  encrypted += cipher.final('hex');
+  var tag = cipher.getAuthTag();
+  return {
+    content: encrypted,
+    tag: tag
+  };
+}
+
+function decrypt(encrypted) {
+  var decipher = crypto.createDecipheriv(algorithm, password, iv)
+  decipher.setAuthTag(encrypted.tag);
+  var dec = decipher.update(encrypted.content, 'hex', 'utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+var hw = encrypt("hello world");
+console.log(hw.tag);
+var buf = Buffer.from(hw.tag).toString('base64');
+console.log(buf);
+buf = Buffer.from(buf, 'base64');
+let text = {};
+text['content'] = hw.content;
+text['tag'] = buf;
+console.log(decrypt(text));
